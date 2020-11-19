dbd307dcd0f30ade581287ffc3362e7a5bffa2c5
VianneyGall <vianneygall@gmail.com>
2020-11-18 22:31:32 -0500

    test WHO_AM_I 1

--- test_I2C/main.cpp
+++ test_I2C/main.cpp
@@ -4,6 +4,7 @@
 #include <wiringPiI2C.h>
 
 #define DEVICE_ID 0x19
+#define WHO_AM_I 0x0FH
 
 int main (int argc, char **argv){
 	// Setup I2C communication
@@ -12,5 +13,7 @@ int main (int argc, char **argv){
         std::cout << "Failed to init I2C communication.\n";
         return -1;
     }
-	std::cout << "I2C communication successfully setup.\n";
+	std::cout << "I2C communication successfully setup.\n" << endl;
+	
+	std::cout << wiringPiI2CReadReg16(fd, WHO_AM_I);
 }
