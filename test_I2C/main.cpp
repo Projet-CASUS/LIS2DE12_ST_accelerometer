6f9b84a132fa9b4d9547c5293e26a6eedfe515ae
VianneyGall <vianneygall@gmail.com>
2020-11-22 13:09:37 -0500

    Adding registers class (first draft)

--- test_I2C/main.cpp
+++ test_I2C/main.cpp
@@ -4,47 +4,8 @@
 #include <thread>
 #include <chrono>
 #include <wiringPiI2C.h>
+#include "registers.h"
 
-//REGISTERS ID
-#define DEVICE_ID 0x19
-#define STATUS_REG_AUX 0x07
-#define OUT_TEMP_L 0x0C
-#define OUT_TEMP_H0x0D
-#define WHO_AM_I 0x0F
-#define CTRL_REG0 0x1E
-#define TEMP_CFG_REG 0x1F
-#define CTRL_REG1 0x20
-#define CTRL_REG2 0x21
-#define CTRL_REG3 0x22
-#define CTRL_REG4 0x23
-#define CTRL_REG5 0x24
-#define CTRL_REG6 0x25
-#define REFERENCE 0x26
-#define STATUS_REG 0x27
-#define FIFO_READ_START 0x28
-#define OUT_X_H 0x29
-#define OUT_Y_H 0x2B
-#define OUT_Z_H 0x2D
-#define FIFO_CTRL_REG 0x2E
-#define FIFO_SRC_REG 0x2F
-#define INT1_CFG 0x30
-#define INT1_SRC 0x31
-#define INT1_THS 0x32
-#define INT1_DURATION 0x33
-#define INT2_CFG 0x34
-#define INT2_SRC 0x35
-#define INT2_THS 0x36
-#define INT2_DURATION 0x37
-#define CLICK_CFG 0x38
-#define CLICK_SRC 0x39
-#define CLICK_THS 0x3A
-#define TIME_LIMIT 0x3B
-#define TIME_LATENCY 0x3C
-#define TIME_WINDOW 0x3D
-#define ACT_THS 0x3E
-#define ACT_DUR 0x3F
-
-int fd;
 
 using namespace std;
 
@@ -62,6 +23,7 @@ int main (int argc, char **argv){
 	cout << "I2C communication successfully setup." << endl;
 	
 	cout << wiringPiI2CReadReg8(fd, WHO_AM_I) << endl;
+	setup_bypass();
 	run_bypass();
 }
 
---
+++ test_I2C/registers.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <thread>
+#include <chrono>
+#include <wiringPiI2C.h>
+#include "registers.h"
+
+Registers::Registers(){
+	fd = wiringPiI2CSetup(DEVICE_ID);
+}
+
+bool Registers::disable_pullup_CTRL_REG0(){
+	
+	return true
+}
+
+bool Registers::enable_TEMP_CFG_REG(){
+	
+	return true
+}
+
+bool Registers::setup_CTRL_REG1_(int setup){
+	
+	return true;
+}
+
+
+
+
+int Registers::read(int reg){
+	return wiringPiI2CReadReg8(fd, reg);
+}
+
+int Registers::write(int reg, int data){
+	return wiringPiI2CWriteReg(fd, reg, data);
+}
+
+int Registers::get_fd(){
+	return fd;
+}
+
+void Registers::set_fd(int FD){
+	fd = FD;
+}
