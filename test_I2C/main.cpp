85f58dff62ee24dedf185465eb2931504078e7eb
VianneyGall <vianneygall@gmail.com>
2020-12-04 21:16:21 -0500

    debug

--- test_I2C/main.cpp
+++ test_I2C/main.cpp
@@ -1,7 +1,7 @@
 //g++ main.cpp registers.cpp -o LIS2DE12_Driver -lwiringPi
 
 #include <iostream>
-#include <bitset>awsds
+#include <bitset>
 #include <thread>
 #include <chrono>
 #include <wiringPiI2C.h>
