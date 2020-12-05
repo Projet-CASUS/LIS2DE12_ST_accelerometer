c38d9238a127e130317d6a2479f4e74812bbb10d
VianneyGall <vianneygall@gmail.com>
2020-12-04 22:40:15 -0500

    debug

--- test_I2C/main.cpp
+++ test_I2C/main.cpp
@@ -33,7 +33,7 @@ int main (int argc, char **argv){
 	bitset<8> y(a);
 	cout << y << endl;	
 	
-	a = reg.read(WHO_AM_I);
+	a = reg.write(CTRL_REG1, 0x40);
 	bitset<8> z(a);
 	cout << z << endl;
 	//run();
