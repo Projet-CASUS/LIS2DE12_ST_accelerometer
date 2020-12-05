370f6d82ab90fac2cb36b8fbe392cffc7882d260
VianneyGall <vianneygall@gmail.com>
2020-12-04 23:37:19 -0500

    debug

--- test_I2C/main.cpp
+++ test_I2C/main.cpp
@@ -34,7 +34,8 @@ bool setup(){
 	
 	reg.write(CTRL_REG1, 0x2F);
 	reg.write(CTRL_REG2, 0x30);
-	int a = reg.read(CTRL_REG2);
+	
+	int a = reg.read(CTRL_REG3);
 	bitset<8> x(a);
 	cout << x << endl;
 	
