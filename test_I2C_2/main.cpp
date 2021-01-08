c8f6397aedc63ef35082d38e037ed324d634f23c
Vianney Gall <vianneygall@gmail.com>
2021-01-07 21:20:34 -0500

    trying a new library

---
+++ test_I2C_2/main.cpp
@@ -0,0 +1,16 @@
+// COMPILATION COMMAND:
+// g++ main.cpp lis2de12_reg.c -o LIS2DE12_Driver
+
+#include <iostream>
+#include <thread>
+#include <chrono>
+#include "lis2de12_reg.h"
+
+
+int main (int argc, char **argv){
+	// Setup I2C communication
+	setup();
+
+	//run();
+	return 0;
+}
